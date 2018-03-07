76d85a82c9878883a37fb89ae30f8cffe76d894d
Filip Petrovič <mail.filip.petrovic@gmail.com>
2018-03-07 19:23:36 +0100

    Added graphics controls to main menu

--- Assets/Scripts/GameManagement/PreferencesManager.cs
+++ Assets/Scripts/GameManagement/PreferencesManager.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.PostProcessing;
 using UnityEngine.UI;
 
 public class PreferencesManager : MonoBehaviour
 {
     [SerializeField] private Slider soundSlider;
     [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider qualitySlider;
+    [SerializeField] private Toggle ambientOcclusionToggle;
+    [SerializeField] private PostProcessingBehaviour postProcessing;
 
     [SerializeField] private AudioSource musicPlayer;
 
@@ -18,6 +22,14 @@ public class PreferencesManager : MonoBehaviour
         {
             musicSlider.onValueChanged.AddListener(delegate { SetMusicLevel(); });
         }
+        if (qualitySlider != null)
+        {
+            qualitySlider.onValueChanged.AddListener(delegate { SetQualityLevel(); });
+        }
+        if (ambientOcclusionToggle != null)
+        {
+            ambientOcclusionToggle.onValueChanged.AddListener(delegate { SetAmbientOcclusion(); });
+        }
 
         if (musicPlayer != null)
         {
@@ -30,11 +42,27 @@ public class PreferencesManager : MonoBehaviour
     private void SetSoundLevel()
     {
         PlayerPrefs.SetFloat("SoundLevel", soundSlider.value);
+        AudioListener.volume = soundSlider.value;
     }
 
     private void SetMusicLevel()
     {
         PlayerPrefs.SetFloat("MusicLevel", musicSlider.value);
+        if (musicPlayer != null)
+        {
+            musicPlayer.volume = musicSlider.value;
+        }
+    }
+
+    private void SetQualityLevel()
+    {
+        PlayerPrefs.SetString("QualityLevel", GetQualityNameForIndex((int)qualitySlider.value));
+    }
+
+    private void SetAmbientOcclusion()
+    {
+        int index = ambientOcclusionToggle.isOn ? 1 : 0;
+        PlayerPrefs.SetInt("AmbientOcclusion", index);
     }
 
     private void LoadPreferences()
@@ -62,5 +90,59 @@ public class PreferencesManager : MonoBehaviour
         {
             musicPlayer.volume = PlayerPrefs.GetFloat("MusicLevel");
         }
+
+        if (!PlayerPrefs.HasKey("QualityLevel"))
+        {
+            PlayerPrefs.SetString("QualityLevel", "Medium");
+        }
+        int qualityIndex = GetQualityIndexForName(PlayerPrefs.GetString("QualityLevel"));
+        QualitySettings.SetQualityLevel(qualityIndex, true);
+        if (qualitySlider != null)
+        {
+            qualitySlider.value = (float)qualityIndex;
+        }
+
+        if (!PlayerPrefs.HasKey("AmbientOcclusion"))
+        {
+            PlayerPrefs.SetInt("AmbientOcclusion", 0);
+        }
+        bool ambientOcclusionSetting = PlayerPrefs.GetInt("AmbientOcclusion") == 1 ? true : false;
+        if (ambientOcclusionToggle != null)
+        {
+            ambientOcclusionToggle.isOn = ambientOcclusionSetting;
+        }
+        postProcessing.profile.ambientOcclusion.enabled = ambientOcclusionSetting;
+    }
+
+    private int GetQualityIndexForName(string name)
+    {
+        string[] qualitySettings = QualitySettings.names;
+
+        for (int i = 0; i < qualitySettings.Length; i++)
+        {
+            if (qualitySettings[i].Equals(name))
+            {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+    private string GetQualityNameForIndex(int index)
+    {
+        if (index == 3)
+        {
+            return "Very High";
+        }
+        else if (index == 2)
+        {
+            return "High";
+        }
+        else if (index == 1)
+        {
+            return "Medium";
+        }
+        return "Low";
     }
 }
