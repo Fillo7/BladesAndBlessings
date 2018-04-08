1b3d47994207a751d42f492ad3246545f77da554
Filip Petrovič <mail.filip.petrovic@gmail.com>
2018-04-08 11:06:42 +0200

    * Improved basic layout for Troll Cave level * Tweaks to environment positions in Forest Ruins level

---
+++ Assets/Scripts/Levels/CaveTeleportation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveTeleportation : MonoBehaviour
+{
+    [SerializeField] private List<Transform> centers = new List<Transform>();
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            other.GetComponent<PlayerHealth>().TakeDamage(100.0f);
+
+            Transform targetArea = GetClosestCenter(other.transform.position);
+            other.transform.SetPositionAndRotation(targetArea.position, Quaternion.identity);
+        }
+    }
+
+    Transform GetClosestCenter(Vector3 position)
+    {
+        float shortestDistance = float.MaxValue;
+        Transform result = null;
+
+        foreach (Transform center in centers)
+        {
+            float currentDistance = Vector3.Distance(position, center.position);
+
+            if (currentDistance < shortestDistance)
+            {
+                shortestDistance = currentDistance;
+                result = center;
+            }
+        }
+
+        return result;
+    }
+}
