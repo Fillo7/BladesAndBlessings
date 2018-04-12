887b0cebce8b88872279f5d7967b3274a218e80c
Filip Petrovič <mail.filip.petrovic@gmail.com>
2018-04-12 18:41:56 +0200

    * Large objects now become transparent when player walks behind them * Other minor tweaks

---
+++ Assets/Scripts/Camera/CameraCollisionHandler.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollisionHandler : MonoBehaviour
+{
+    private float currentTransparency = 0.5f;
+    private float targetTransparency = 0.5f;
+    private float transparencyDuration = 0.1f;
+
+    private Renderer[] objectRenderers = null;
+    private List<Color> oldColors = new List<Color>();
+
+    void Awake()
+    {
+        objectRenderers = GetComponentsInChildren<Renderer>();
+
+        for (int i = 0; i < objectRenderers.Length; i++)
+        {
+            oldColors.Add(objectRenderers[i].material.color);
+        }
+    }
+
+    void Update()
+    {
+        if (currentTransparency < 1.0f)
+        {
+            for (int i = 0; i < objectRenderers.Length; i++)
+            {
+                Material material = objectRenderers[i].material;
+                material.SetFloat("_Mode", 3);
+                material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                material.SetInt("_ZWrite", 0);
+                material.DisableKeyword("_ALPHATEST_ON");
+                material.DisableKeyword("_ALPHABLEND_ON");
+                material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+                material.renderQueue = 3000;
+
+                Color materialColor = material.color;
+                materialColor.a = currentTransparency;
+                material.color = materialColor;
+                objectRenderers[i].material = material;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < objectRenderers.Length; i++)
+            {
+                Material material = objectRenderers[i].material;
+                material.SetFloat("_Mode", 0);
+                material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                material.SetInt("_ZWrite", 1);
+                material.DisableKeyword("_ALPHATEST_ON");
+                material.DisableKeyword("_ALPHABLEND_ON");
+                material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                material.renderQueue = -1;
+
+                material.color = oldColors[i];
+                objectRenderers[i].material = material;
+            }
+            Destroy(this);
+        }
+        currentTransparency += ((1.0f - targetTransparency) * Time.deltaTime) / transparencyDuration;
+    }
+
+    public void SetTransparency()
+    {
+        currentTransparency = targetTransparency;
+    }
+}
--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -5,10 +5,27 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private float smoothing = 3.0f;
     private Vector3 offset;
+    private int environmentMask;
 
     void Start()
     {
         offset = transform.position - target.position;
+        environmentMask = LayerMask.GetMask("Environment");
+    }
+
+    void Update()
+    {
+        RaycastHit[] hits = Physics.RaycastAll(transform.position, transform.forward, offset.magnitude, environmentMask);
+
+        foreach (RaycastHit hit in hits)
+        {
+            CameraCollisionHandler collisionHandler = hit.collider.gameObject.GetComponent<CameraCollisionHandler>();
+            if (collisionHandler == null)
+            {
+                collisionHandler = hit.collider.gameObject.AddComponent<CameraCollisionHandler>();
+            }
+            collisionHandler.SetTransparency();
+        }
     }
 
     void FixedUpdate()
